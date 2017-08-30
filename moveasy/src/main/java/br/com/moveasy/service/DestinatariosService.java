f5bf6bb5d7e6ca85b91be2e5e8c1fc59ca75b32c
Noturno <Noturno@192.168.7.13>
2017-08-29 21:23:35 -0300

    Service

--- moveasy/src/main/java/br/com/moveasy/service/DestinatariosService.java
+++ moveasy/src/main/java/br/com/moveasy/service/DestinatariosService.java
@@ -1,8 +1,35 @@
 package br.com.moveasy.service;
 
-import br.com.moveasy.model.Tipo_servico;
+import br.com.moveasy.model.Destinatarios;
 
 public class DestinatariosService {
 
+	public void cadastrar(String nome_dest, Endereco endereco_dest) throws SQLException{
+		try (Connection conexao = new ConnectionPoolOracle().getConnection()) {
+			new DestinatariosDAO(conexao).cadastrar(nome_dest, endereco_dest);
+		}
+	}
+	
+	public List<Destinatarios> listar() throws SQLException{
+		try (Connection conexao = new ConnectionPoolOracle().getConnection()) {
+            return new DestinatariosDAO(conexao).lista();
+		}
+	}
+	
+	public void editar(Destinatarios destinatarios) throws SQLException{
+		try (Connection conexao = new ConnectionPoolOracle().getConnection()) {
+			new DestinatariosDAO(conexao).editar(destinatarios.getCod_dest());
+		}
+	}
+	
+	public void deletar(Destinatarios destinatarios) throws SQLException{
+		try (Connection conexao = new ConnectionPoolOracle().getConnection()) {
+			new DestinatariosDAO(conexao).deletar(destinatarios.getCod_dest());
+		}
+	}
 
+	
+	
 }
+
+
